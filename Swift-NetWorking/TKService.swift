d9cd788424d27435c74fcee627c2955272616a8c
FlyElephant <keso_xiang@talkmate.com>
2016-11-25 19:03:57 +0800

    Update~

---
+++ Swift-NetWorking/TKService.swift
@@ -0,0 +1,13 @@
+//
+//  TKService.swift
+//  IPad_TalkmateGame
+//
+//  Created by FlyElephant on 16/11/21.
+//  Copyright © 2016年 Talkmate. All rights reserved.
+//
+
+import UIKit
+
+class TKService: NSObject {
+
+}
